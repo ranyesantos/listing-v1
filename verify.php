47bed7c9796a1922c92b76cc3c5793d40faaa076
Ranie <143940800+ranyesantos@users.noreply.github.com>
2023-09-03 20:53:52 -0300

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,14 +1,14 @@
 <?php  
-if (isset($_POST['user'], $_POST['password'])) {
-    $user = $_POST['user'];
-    $password = $_POST['password'];
-}  
-     
-if ($user != 'admin' or $password != 'admin'){
-    header ('Location:error.html');
-}
-else {
-    header('Location:logedin.html');
-}
+    if (isset($_POST['user'], $_POST['password'])) {
+        $user = $_POST['user'];
+        $password = $_POST['password'];
+    }  
+         
+    if ($user != 'admin' or $password != 'admin'){
+        header ('Location:error.html');
+    }
+    else {
+        header('Location:logedin.html');
+    }
 
 ?>
